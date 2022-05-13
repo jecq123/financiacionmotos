5fd6a404d88197ab0eefa881a35b9c5db63024d2
felipe-u <felipe_uv@outlook.com>
2022-05-13 15:25:46 -0500

    Creación capa Converter

---
+++ back-end/sistemainventario/src/main/java/co/com/uco/sistemainventario/converter/CreditoConverter.java
@@ -0,0 +1,70 @@
+package co.com.uco.sistemainventario.converter;
+
+
+import co.com.uco.sistemainventario.comando.ComandoCredito;
+import co.com.uco.sistemainventario.entidad.Credito;
+import co.com.uco.sistemainventario.validador.ValidadorArgumento;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreditoConverter {
+
+    public static final String EL_ID_DEL_CLIENTE_ES_OBLIGATORIO = "El id del cliente es obligatorio";
+    public static final String EL_ID_DEL_VENDEDOR_ES_OBLIGATORIO = "El id del vendedor es obligatorio";
+    public static final String EL_ID_DE_LA_MOTO_ES_OBLIGATORIO = "El id de la moto es obligatorio";
+
+    public static final String EL_VALOR_DE_LA_MOTO_ES_OBLIGATORIO = "El valor de la moto es obligatorio";
+    public static final String EL_VALOR_DE_LA_CUOTA_INICIAL_ES_OBLIGATORIO = "El valor de la cuota inicial es obligatorio";
+    public static final String SE_REQUIERE_EL_NUMERO_DE_CUOTAS = "Se requiere el número de cuotas";
+    public static final String LA_FECHA_DEL_CREDITO_ES_OBLIGATORIA = "La fecha del crédito es obligatoria";
+    public static final String EL_ID_DEL_CREDITO_ES_OBLIGATORIO = "El id del crédito es obligatorio";
+    public static final String EL_ID_DEL_CREDITO_ES_INVALIDO = "El id del crédito es inválido";
+
+    public Credito crear(ComandoCredito comandoCredito) {
+        Credito credito = new Credito();
+        ValidadorArgumento.validarObligatorio(comandoCredito.getIdCliente(), EL_ID_DEL_CLIENTE_ES_OBLIGATORIO);
+        ValidadorArgumento.validarObligatorio(comandoCredito.getIdVendedor(), EL_ID_DEL_VENDEDOR_ES_OBLIGATORIO);
+        ValidadorArgumento.validarObligatorio(comandoCredito.getIdMoto(), EL_ID_DE_LA_MOTO_ES_OBLIGATORIO);
+        ValidadorArgumento.validarObligatorio(comandoCredito.getValorMoto(), EL_VALOR_DE_LA_MOTO_ES_OBLIGATORIO);
+        ValidadorArgumento.validarObligatorio(comandoCredito.getValorCuotaInicial(), EL_VALOR_DE_LA_CUOTA_INICIAL_ES_OBLIGATORIO);
+        ValidadorArgumento.validarObligatorio(comandoCredito.getNumeroCuotas(), SE_REQUIERE_EL_NUMERO_DE_CUOTAS);
+        ValidadorArgumento.validarObligatorio(comandoCredito.getFecha(), LA_FECHA_DEL_CREDITO_ES_OBLIGATORIA);
+        credito.getCliente().setIdCliente(comandoCredito.getIdCliente());
+        credito.getVendedor().setIdVendedor(comandoCredito.getIdVendedor());
+        credito.getMoto().setIdMoto(comandoCredito.getIdMoto());
+        credito.setValorMoto(comandoCredito.getValorMoto());
+        credito.setValorCuotaInicial(comandoCredito.getValorCuotaInicial());
+        credito.setNumeroCuotas(comandoCredito.getNumeroCuotas());
+        credito.setFecha(comandoCredito.getFecha());
+        return credito;
+    }
+
+    public Credito editar(ComandoCredito comandoCredito) {
+        Credito credito = new Credito();
+        ValidadorArgumento.validarObligatorio(comandoCredito.getIdCredito(), EL_ID_DEL_CREDITO_ES_OBLIGATORIO);
+        ValidadorArgumento.validarObligatorio(comandoCredito.getIdCliente(), EL_ID_DEL_CLIENTE_ES_OBLIGATORIO);
+        ValidadorArgumento.validarObligatorio(comandoCredito.getIdVendedor(), EL_ID_DEL_VENDEDOR_ES_OBLIGATORIO);
+        ValidadorArgumento.validarObligatorio(comandoCredito.getIdMoto(), EL_ID_DE_LA_MOTO_ES_OBLIGATORIO);
+        ValidadorArgumento.validarObligatorio(comandoCredito.getValorMoto(), EL_VALOR_DE_LA_MOTO_ES_OBLIGATORIO);
+        ValidadorArgumento.validarObligatorio(comandoCredito.getValorCuotaInicial(), EL_VALOR_DE_LA_CUOTA_INICIAL_ES_OBLIGATORIO);
+        ValidadorArgumento.validarObligatorio(comandoCredito.getNumeroCuotas(), SE_REQUIERE_EL_NUMERO_DE_CUOTAS);
+        ValidadorArgumento.validarObligatorio(comandoCredito.getFecha(), LA_FECHA_DEL_CREDITO_ES_OBLIGATORIA);
+        credito.setidCredito(comandoCredito.getIdCredito());
+        credito.getCliente().setIdCliente(comandoCredito.getIdCliente());
+        credito.getVendedor().setIdVendedor(comandoCredito.getIdVendedor());
+        credito.getMoto().setIdMoto(comandoCredito.getIdMoto());
+        credito.setValorMoto(comandoCredito.getValorMoto());
+        credito.setValorCuotaInicial(comandoCredito.getValorCuotaInicial());
+        credito.setNumeroCuotas(comandoCredito.getNumeroCuotas());
+        credito.setFecha(comandoCredito.getFecha());
+        return credito;
+    }
+
+    public Credito borrar(Integer idCredito) {
+        ValidadorArgumento.validarObligatorio(idCredito, EL_ID_DEL_CRÉDITO_ES_OBLIGATORIO);
+        ValidadorArgumento.validarPositivo(idCredito, EL_ID_DEL_CREDITO_ES_INVALIDO);
+        Credito credito = new Credito();
+        credito.setIdCredito(idCredito);
+        return credito;
+    }
+}
---
+++ back-end/sistemainventario/src/main/java/co/com/uco/sistemainventario/converter/MotoConverter.java
@@ -0,0 +1,58 @@
+package co.com.uco.sistemainventario.converter;
+
+import co.com.uco.sistemainventario.comando.ComandoMoto;
+import co.com.uco.sistemainventario.entidad.Moto;
+import co.com.uco.sistemainventario.validador.ValidadorArgumento;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MotoConverter {
+
+    public static final String LA_REFERENCIA_DE_LA_MOTO_ES_OBLIGATORIA = "La referencia de la moto es obligatoria";
+    public static final String LA_MARCA_DE_LA_MOTO_ES_OBLIGATORIA = "La marca de la moto es obligatoria";
+    public static final String EL_MODELO_DE_LA_MOTO_ES_OBLIGATORIO = "El modelo de la moto es obligatorio";
+    public static final String EL_CILINDRAJE_DE_LA_MOTO_ES_OBLIGATORIO = "El cilindraje de la moto es obligatorio";
+    public static final String EL_PRECIO_DE_LA_MOTO_ES_OBLIGATORIO = "El precio de la moto es obligatorio";
+    public static final String EL_ID_DE_LA_MOTO_ES_OBLIGATORIO = "El id de la moto es obligatorio";
+    public static final String EL_ID_DE_LA_MOTO_NO_ES_VALIDO = "El id de la moto no es válido";
+
+    public Moto crear(ComandoMoto comandoMoto) {
+        Moto moto = new Moto();
+        ValidadorArgumento.validarObligatorio(comandoMoto.getReferencia(), LA_REFERENCIA_DE_LA_MOTO_ES_OBLIGATORIA);
+        ValidadorArgumento.validarObligatorio(comandoMoto.getMarca(), LA_MARCA_DE_LA_MOTO_ES_OBLIGATORIA);
+        ValidadorArgumento.validarObligatorio(comandoMoto.getModelo(), EL_MODELO_DE_LA_MOTO_ES_OBLIGATORIO);
+        ValidadorArgumento.validarObligatorio(comandoMoto.getCilindraje(), EL_CILINDRAJE_DE_LA_MOTO_ES_OBLIGATORIO);
+        ValidadorArgumento.validarObligatorio(comandoMoto.getPrecio(), EL_PRECIO_DE_LA_MOTO_ES_OBLIGATORIO);
+        moto.setReferencia(comandoMoto.getReferencia());
+        moto.setMarca(comandoMoto.getMarca());
+        moto.setModelo(comandoMoto.getModelo());
+        moto.setCilindraje(comandoMoto.getCilindraje());
+        moto.setPrecio(comandoMoto.getPrecio());
+        return moto;
+    }
+
+    public Moto editar(ComandoMoto comandoMoto) {
+        Moto moto = new Moto();
+        ValidadorArgumento.validarObligatorio(comandoMoto.getIdMoto(), EL_ID_DE_LA_MOTO_ES_OBLIGATORIO);
+        ValidadorArgumento.validarObligatorio(comandoMoto.getReferencia(), LA_REFERENCIA_DE_LA_MOTO_ES_OBLIGATORIA);
+        ValidadorArgumento.validarObligatorio(comandoMoto.getMarca(), LA_MARCA_DE_LA_MOTO_ES_OBLIGATORIA);
+        ValidadorArgumento.validarObligatorio(comandoMoto.getModelo(), EL_MODELO_DE_LA_MOTO_ES_OBLIGATORIO);
+        ValidadorArgumento.validarObligatorio(comandoMoto.getCilindraje(), EL_CILINDRAJE_DE_LA_MOTO_ES_OBLIGATORIO);
+        ValidadorArgumento.validarObligatorio(comandoMoto.getPrecio(), EL_PRECIO_DE_LA_MOTO_ES_OBLIGATORIO);
+        moto.setIdMoto(comandoMoto.getIdMoto());
+        moto.setReferencia(comandoMoto.getReferencia());
+        moto.setMarca(comandoMoto.getMarca());
+        moto.setModelo(comandoMoto.getModelo());
+        moto.setCilindraje(comandoMoto.getCilindraje());
+        moto.setPrecio(comandoMoto.getPrecio());
+        return moto;
+    }
+
+    public Moto borrar(Integer idMoto) {
+        ValidadorArgumento.validarObligatorio(idMoto, EL_ID_DE_LA_MOTO_ES_OBLIGATORIO);
+        ValidadorArgumento.validarPositivo(idMoto, EL_ID_DE_LA_MOTO_NO_ES_VALIDO);
+        Moto moto = new Moto();
+        moto.setIdMoto(idMoto);
+        return moto;
+    }
+}
